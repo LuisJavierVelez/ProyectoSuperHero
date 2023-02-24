7eec86cfd5b03c0e12bb5b493d81a017be2c2eb4
unknown <ljvelez89571@umanizales.edu.co>
2023-02-23 21:54:19 -0500

    Archivo Index configuracion de conexion a la base de datos

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ require("dotenv").config();
 app.listen(process.env.PORT_PC, () => 
     console.log(`Connect in the port_PC ${process.env.PORT_PC}`)
 );
+
+mongoose.set("strictQuery", false);
+
+mongoose
+    .connect(process.env.STRING_CONNECTION)
+    .then(() => console.log("Success connection"))
+    .catch((err) => console.error(err));
